0e36ab35b34d2afe307bac1ea010373dacd52ee2
cariolano <cariolanosolar@gmail.com>
2019-11-24 21:31:35 -0200

    atualziando pacote e classe em TS

---
+++ negocio.java
@@ -0,0 +1,13 @@
+
+public class negocio {
+
+	public negocio() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
